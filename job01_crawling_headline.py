a5c6ea917e3a4d687dd5435ed1880e2090e798d8
Mike <michaelmw@naver.com>
2025-04-16 11:30:31 +0900

    My first commit

--- job01_crawling_headline.py
+++ job01_crawling_headline.py
@@ -9,14 +9,18 @@ df_titles = pd.DataFrame()
 
 url = 'https://news.naver.com/section/100'
 resp = requests.get(url)
-print(resp)
+#print(resp)
 
 soup = BeautifulSoup(resp.text, 'html.parser')
-print(list(soup))
+#print(list(soup))
 
 title_tags = soup.select('.sa_text_strong')  #컴머는 클래스 의미
-title = title_tags.text                      #문자열만 출력
-print(list(title_tags))
+titles = []
+
+for tag in title_tags:
+    titles.append(tag.text)                  #문자열만 출력
+
+print(titles)
 
 
 
