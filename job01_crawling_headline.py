cce1bb15b56dd7ae2918bd65101aeeb73a33a6f7
Mike <michaelmw@naver.com>
2025-04-16 11:56:27 +0900

    My first commit

--- job01_crawling_headline.py
+++ job01_crawling_headline.py
@@ -7,21 +7,33 @@ import datetime
 category = ['politics', 'Economic', 'social', 'Culture', 'world', 'IT']
 df_titles = pd.DataFrame()
 
-url = 'https://news.naver.com/section/100'
-resp = requests.get(url)
-#print(resp)
+for i in range(6):
 
-soup = BeautifulSoup(resp.text, 'html.parser')
-#print(list(soup))
+    url = 'https://news.naver.com/section/10{}'.format(i)
 
-title_tags = soup.select('.sa_text_strong')  #컴머는 클래스 의미
-titles = []
+    resp = requests.get(url)
+    #print(resp)
 
-for tag in title_tags:
-    titles.append(tag.text)                  #문자열만 출력
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    #print(list(soup))
 
-print(titles)
+    title_tags = soup.select('.sa_text_strong')  #컴머는 클래스 의미
 
+    titles = []
+    for tag in title_tags:
+        titles.append(tag.text)                  #문자열만 출력
 
+    print(titles)
 
+    df_section_titles = pd.DataFrame(titles, columns=['titles'])
+    df_section_titles['category'] = category[i]
+    df_titles = pd.concat([df_titles, df_section_titles],
+                          axis='rows', ignore_index=True)
 
+#print(df_section_titles.head())
+#df_section_titles.info() #정보 보여짐
+df_titles.info()
+print(df_titles['category'].value_counts())
+
+df_titles.to_csv('./crawling_data/naver_headline_news_{}.csv'.format(
+    datetime.datetime.now().strftime('%Y%m%d')), index=False)
